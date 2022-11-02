0d5ea13576b558ddfddc80fbbc5490267b4348a5
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-01 19:53:41 -0600

    sin sección de marcas

--- index.php
+++ index.php
@@ -281,16 +281,7 @@
             </div>
         </div>
     </section>
-    <section class="py-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 col-md-3"><a href="#"><img class="img-fluid d-block mx-auto" src="/assets/img/clients/creative-market.jpg"></a></div>
-                <div class="col-sm-6 col-md-3"><a href="#"><img class="img-fluid d-block mx-auto" src="/assets/img/clients/designmodo.jpg"></a></div>
-                <div class="col-sm-6 col-md-3"><a href="#"><img class="img-fluid d-block mx-auto" src="/assets/img/clients/envato.jpg"></a></div>
-                <div class="col-sm-6 col-md-3"><a href="#"><img class="img-fluid d-block mx-auto" src="/assets/img/clients/themeforest.jpg"></a></div>
-            </div>
-        </div>
-    </section>
+
     <section id="contacto" style="background-image:url('/assets/img/map-image.png');">
         <div class="container">
             <div class="row">
