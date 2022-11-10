566ba1605b72644327e24c34d8d059038a22669f
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-10 00:11:12 -0600

    creadas y formateadas políticas de garantía

--- políticas-garantía.php
+++ políticas-garantía.php
@@ -1,12 +1,16 @@
-POLÍTICAS DE GARANTÍA
-Términos y condiciones:
-Agradecemos su confianza y hacemos de su conocimiento las siguientes condiciones de servicio.
--Después de 30 días los equipos pueden ser usados como remate o refacción sin responsabilidad de parte nuestra.
--Retire chip, memoria y funda no nos hacemos responsables de tales perdidas.
--En equipos mojados, software, daños por mal uso, lógicas reparadas o intervenidos NO HAY GARANTIA.
--La entrega del equipo será solo con nota o identificación.
--Los reemplazos de piezas cuentan con 30 días de garantía contra defectos de fábrica. NO APLICA si el equipo presenta daño por golpe humedad o mal uso. Pueden existir variaciones entre la pieza original y el reemplazo como logos, tonalidad de color, material etc.
--Piezas genéricas NO HAY GARANTIA.
--Los tiempos para liberación por código y vía servidor son establecidos por proveedores terceros en raras ocasiones pueden retrasarse. No habrá reembolso hasta completada la orden. La liberación de compañía NO elimina el reporte de robo o extravió.
--El tiempo de entrega puede retrasarse cuando la refacción viene de proveedores externos.
--En equipos mojados el costo de servicio es de $150.00 pesos únicamente es por el servicio de limpieza y secado NO incluye la reparación si algún componente esta dañado, el hecho que siga sin encender o siga fallando NO lo libra de pagar el costo del servicio.
+<h2 class="text-uppercase">Políticas de garantía</h2>
+<h3 class="text-muted section-subheading">
+    Términos y condiciones:
+    Agradecemos su confianza y hacemos de su conocimiento las siguientes condiciones de servicio:
+</h3>
+<ol class="politica-garantia-lista text-muted section-subheading">
+    <li class="politica-garantia-elemento-lista">-Después de 30 días los equipos pueden ser usados como remate o refacción sin responsabilidad de parte nuestra.</li>
+    <li class="politica-garantia-elemento-lista">-Retire chip, memoria y funda no nos hacemos responsables de tales perdidas.</li>
+    <li class="politica-garantia-elemento-lista">-En equipos mojados, software, daños por mal uso, lógicas reparadas o intervenidos NO HAY GARANTIA.</li>
+    <li class="politica-garantia-elemento-lista">-La entrega del equipo será solo con nota o identificación.</li>
+    <li class="politica-garantia-elemento-lista">-Los reemplazos de piezas cuentan con 30 días de garantía contra defectos de fábrica. NO APLICA si el equipo presenta daño por golpe humedad o mal uso. Pueden existir variaciones entre la pieza original y el reemplazo como logos, tonalidad de color, material etc.</li>
+    <li class="politica-garantia-elemento-lista">-Piezas genéricas NO HAY GARANTIA.</li>
+    <li class="politica-garantia-elemento-lista">-Los tiempos para liberación por código y vía servidor son establecidos por proveedores terceros en raras ocasiones pueden retrasarse. No habrá reembolso hasta completada la orden. La liberación de compañía NO elimina el reporte de robo o extravió.</li>
+    <li class="politica-garantia-elemento-lista">-El tiempo de entrega puede retrasarse cuando la refacción viene de proveedores externos.</li>
+    <li class="politica-garantia-elemento-lista">-En equipos mojados el costo de servicio es de $150.00 pesos únicamente es por el servicio de limpieza y secado NO incluye la reparación si algún componente esta dañado, el hecho que siga sin encender o siga fallando NO lo libra de pagar el costo del servicio.</li>
+</ol>
