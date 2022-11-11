ad1f84b38486c8d117d401c56f73aad10a394293
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-11-11 06:42:44 -0600

    update modales.php

--- modales.php
+++ modales.php
@@ -5,9 +5,9 @@
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
                         <div class="modal-body">
-                            <h2 class="text-uppercase">Reparación</h2>
-                            <p class="text-muted item-intro">Complemento reparación.</p><img class="img-fluid d-block mx-auto" src="/assets/img/iOS-Android.webp">
-                            <p>Descripción y detalle completo sobre la reparación.</p>
+                            <h2 class="text-uppercase">Reparación Multimarca</h2>
+                            <p class="text-muted item-intro">Calidad y variedad</p><img class="img-fluid d-block mx-auto" src="/assets/img/iOS-Android.webp">
+                            <p>Podemos reparar cualquier marca modelo o variante de celular, tableta, computadora o smartwatch incluyendo Android o Apple de cualquier marca comercial (Alcatel, Samsung, Motorola, Huawei, LG,  Xiaomi,Oppo, Realme, Zte, iPhone, iPad, Lenovo, Asus, Sony, Hp, Acer, Toshiba, Dell, AppleWatch, etc.) Y marcas no comerciales (equipos clones, de imitación y equipos chinos ).</p>
                             <ul class="list-unstyled">
                                 <li>Date: January 2017</li>
                                 <li>Client: Threads</li>
